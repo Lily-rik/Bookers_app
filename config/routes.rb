d12df23c8cca16e235714899b5d39857e8020b51
EC2 Default User <violet794.py16@gmail.com>
2021-04-10 01:06:04 +0000

    [add]4-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :books
+
+  get 'homes' => 'homes#index', as: 'homes'
+  root to: 'homes#index'
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
