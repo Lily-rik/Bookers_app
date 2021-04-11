e3c5eef66636d70fd8558d6040a48a4594fa4a70
EC2 Default User <violet794.py16@gmail.com>
2021-04-11 23:50:26 +0000

    [add]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,12 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
+    @books = Book.all
   end
 
   def show
   end
 
-  def new
-    @book = Book.new
-  end
 
   def create
     @book = Book.new(book_params)
